6fab2bcad2565288fc41293208dc821dca23e21e
dimas-flj <dimasflj@gmail.com>
2020-01-04 13:55:02 -0300

    Reconfigurando arquivo Procfile 12

--- src/main/java/com/learn/cursomc/SpringBootIonicBackendApplication.java
+++ src/main/java/com/learn/cursomc/SpringBootIonicBackendApplication.java
@@ -1,11 +1,23 @@
 package com.learn.cursomc;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootIonicBackendApplication {
+public class SpringBootIonicBackendApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootIonicBackendApplication.class, args);
+	}
+	
+	public void run(String... args) throws Exception {}
+	
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("GMT-3"));
 	}
 }
